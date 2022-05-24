392366e6bb25894acb4fe05c580bd37c8ba46228
jose.gomes <jose.gomes@tmov.com.br>
2022-05-24 17:09:44 -0300

    finished project

---
+++ components/photo.js
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import React from 'react';
+import styles from '../styles/Photo.module.scss'
+import { getRandomInt } from '../util/util';
+export default function Photo({ photoName, alt, left, deg, duration, delay, size, subtitle }) {
+
+  return (
+    <div className={styles.photo} style={{
+                                           left,
+                                           transform: `rotate(${getRandomInt(deg - 6, deg + 6)}deg)`,
+                                           animationName: 'moveDown',
+                                           animationDuration: `${duration}s`,
+                                           animationDelay: `${delay}s`,
+                                           animationTimingFunction: 'linear',
+                                           animationFillMode: 'forwards'
+                                          }}>
+       <span className={styles.subtitle}>{subtitle}</span>
+       <Image src={`/${photoName}`} alt={alt} width={size ?? 500} height={size ?? 500} />
+    </div>
+  )
+}
